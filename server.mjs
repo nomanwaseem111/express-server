2597bd517feae29ef889dc9350f4745154901436
Noman Waseem <nomanwaseem111@gmail.com>
2022-08-17 00:38:59 +0500

    few changes

--- server.mjs
+++ server.mjs
@@ -3,7 +3,7 @@ import cors from 'cors'
 const app = express()
 app.use(express.json())
 app.use(cors())
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/user', (req, res) => {
   res.send('Hello World!')
